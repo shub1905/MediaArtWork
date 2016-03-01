2d4bd73d315b692a54be7d4323f8e33fdf748214
shub1905 <shub1905@gmail.com>
2016-02-29 20:56:50 -0500

    download complete

--- tagwriter.py
+++ tagwriter.py
@@ -1,10 +1,37 @@
 import fileinfo
 import os
+import sys
+import json
+import requests
 
-rootdir = '/Users/satan/Music/iTunes/iTunes\ Media/Music/'
+rootdir = '/Users/satan/Music/iTunes/iTunes Media/Music/'
+API = 'https://itunesartwork.dodoapps.io/?query={}&entity=album&country=us'
+output_folder = 'output/'
+
+def download_file(album):
+    url_download = album['url']
+    local_filename = album['title']+'.jpg'
+    local_filename = output_folder+local_filename.replace('/','_')
+    print local_filename
+
+    r = requests.get(url_download, stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024): 
+            if chunk:
+                f.write(chunk)
+    return local_filename
+
+def get_album_details(album_name):
+    url = API.format(album_name)
+    r = requests.get(url)
+    album_details = r.json()
+    for album in album_details:
+        download_file(album)
 
 for root, subdirs, files in os.walk(rootdir):
-	print 'heh', subdirs
-	for sub in subdirs:
-	    for info in fileinfo.listDirectory(sub, [".mp3"]):
-	        print "\n".join(["%s=%s" % (k, v) for k, v in info.items()])
+    for sub in subdirs:
+        for info in fileinfo.listDirectory(root + '/' + sub, [".mp3"]):
+            print "\n".join(["%s=%s" % (k, v) for k, v in info.items()])
+            for k,v in info.items():
+                if k == 'album':
+                    get_album_details(v)
